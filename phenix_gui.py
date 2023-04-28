00e69d275ee7a4bc034e1d2ab7510ac623b0c323
Adrian Barrera-Velasquez <abarre16@my.smccd.edu>
2023-04-27 23:58:02 -0700

    Tweaked gui
    Removed the custom tkinter for now while I learn how to use vanilla
    tkinter.

--- phenix_gui.py
+++ phenix_gui.py
@@ -1,16 +1,28 @@
-import customtkinter as ctk
+from phenix_experiment import PhenixExperiment
 
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
 
-def get_exp_dir():
-    exp_dir = filedialog.askdirectory(title="test..?")
+class PhenixGui:
+    def __init__(self):
+        # Initialize the variable to none and assign later, is this best practice?
+        self.experiment = None
+        self.root = Tk()
+        self.frame = ttk.Frame(master=self.root)
+        self.frame.grid(column=0,row=0,sticky=(N,W,E,S))
+
+        self.dir_button = ttk.Button(master=self.frame, text = "Choose experiment...", command = self.load_experiment)
+        self.dir_button.grid()
+
+        # Main loop
+        self.root.mainloop()
+
+
+    def load_experiment(self):
+        exp_dir = filedialog.askdirectory(title="test..?")
+        self.experiment = PhenixExperiment(exp_dir)
+        print(self.experiment)
 
 if __name__ == "__main__":
-    root = ctk.CTk()
-    root.geometry("640x480")
-
-    dir_button = ctk.CTkButton(master=root, text = "Choose experiment", command = get_exp_dir)
-    dir_button.pack()
-    root.mainloop()
+    gui = PhenixGui()
